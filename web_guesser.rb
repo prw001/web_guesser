2b3fa767fa46350521cf8c85b68d71b166dd5786
Paul Winter <pwinter001@gmail.com>
2018-08-30 20:53:10 -0700

    Add feature color indicator for guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,25 +3,28 @@ require 'sinatra/reloader'
 set :sessions, true
 
 X = rand(100)
+bgcolor = 'white'
 
 def check_guess(guess)
 	if !(guess)
 		return ''
 	elsif guess.to_i >= X + 5
-		return "Way too high!"
+		return ["Way too high!", 'red']
 	elsif guess.to_i > X && guess.to_i < X + 5
-		return "Too High!"
+		return ["Too High!", 'pink']
 	elsif guess.to_i <= X - 5
-		return "Way too low!"
+		return ["Way too low!", 'red']
 	elsif guess.to_i < X && guess.to_i > X - 5
-		return "Too low!"
+		return ["Too low!", 'pink']
 	else
-		return "You got it! The Secret Number was #{X}"
+		return ["You got it! The Secret Number was #{X}", 'green']
 	end
 end
 
 get '/' do 
 	guess = params["guess"]
-	message = check_guess(guess)
-	erb :index, :locals => {:x => X, :message => message}
+	result = check_guess(guess)
+	message = result[0]
+	bgcolor = result[1]
+	erb :index, :locals => {:x => X, :message => message, :bgcolor => bgcolor}
 end
