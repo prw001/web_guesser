68cb9b88718c545182728d80e9068e5d3da8d416
Paul Winter <pwinter001@gmail.com>
2018-08-30 21:12:16 -0700

    add guess limit (5) and reset

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,22 +2,39 @@ require 'sinatra'
 require 'sinatra/reloader'
 set :sessions, true
 
-X = rand(100)
+@@X = rand(100)
 bgcolor = 'white'
+@@guesses = 5
+
+
 
 def check_guess(guess)
-	if !(guess)
-		return ''
-	elsif guess.to_i >= X + 5
-		return ["Way too high!", 'red']
-	elsif guess.to_i > X && guess.to_i < X + 5
-		return ["Too High!", 'pink']
-	elsif guess.to_i <= X - 5
-		return ["Way too low!", 'red']
-	elsif guess.to_i < X && guess.to_i > X - 5
-		return ["Too low!", 'pink']
+	message = ''
+	case
+	when !(guess)
+		values = ['Guess my secret number!', 'white']
+	when guess.to_i >= @@X + 5
+		@@guesses -= 1
+		values = ['Way too high!', 'red']
+	when guess.to_i > @@X && guess.to_i < @@X + 5
+		@@guesses -= 1
+		values = ['Too high!', 'pink']
+	when guess.to_i <= @@X - 5
+		@@guesses -= 1
+		values = ['Way too low!', 'red']
+	when guess.to_i < @@X && guess.to_i > @@X - 5
+		@@guesses -= 1
+		values = ['Too low!', 'pink']
 	else
-		return ["You got it! The Secret Number was #{X}", 'green']
+		values = ['You got it!', 'green']
+	end
+	unless @@guesses == 0
+		return values
+	else
+		@@guesses = 5
+		@@X = rand(100)
+		values = ["That's too bad, you didn't guess my number. I'll pick a new one.", 'gray']
+		return values
 	end
 end
 
@@ -26,5 +43,5 @@ get '/' do
 	result = check_guess(guess)
 	message = result[0]
 	bgcolor = result[1]
-	erb :index, :locals => {:x => X, :message => message, :bgcolor => bgcolor}
+	erb :index, :locals => {:x => @@X, :message => message, :bgcolor => bgcolor}
 end
